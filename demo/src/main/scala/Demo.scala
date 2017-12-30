852d1d9edddf3c170ea11b179fc2998f39f48ead
Sergey Kozlov <mail.sergey.kozlov@gmail.com>
2017-12-30 18:44:23 +0700

    `Demo`: `require` instead of `println`

--- demo/src/main/scala/Demo.scala
+++ demo/src/main/scala/Demo.scala
@@ -4,51 +4,51 @@ object Demo extends App{
 	import Rank._
 
 	val suits: List[Suit] = List(♠, ♥, ♦, ♣)
-	println(suits mkString " ") //♠ ♥ ♦ ♣
+	require((suits mkString " ") == "♠ ♥ ♦ ♣")
 
-	println(List(♠, ♥, ♦, ♣) == List(Spades, Hearts, Diamonds, Clubs)) //true
+	require(List(♠, ♥, ♦, ♣) == List(Spades, Hearts, Diamonds, Clubs))
 
 	val ranks: List[Rank] = List(A, K, Q, J, T, `9`, `8`, `7`, `6`, `5`, `4`, `3`, `2`)
-	println(ranks mkString " ") //A K Q J T 9 8 7 6 5 4 3 2
+	require((ranks mkString " ") == "A K Q J T 9 8 7 6 5 4 3 2")
 
-	println(A > K) //true
+	require(A > K)
 
 	val unorderedRanks = scala.util.Random.shuffle(ranks)
 	println(unorderedRanks mkString " ") //"Q 8 2 J 7 4 K 9 3 A 5 T 6" or in another order
-	println(unorderedRanks.sorted mkString " ") //2 3 4 5 6 7 8 9 T J Q K A
+	require((unorderedRanks.sorted mkString " ") == "2 3 4 5 6 7 8 9 T J Q K A")
 
-	println(List(A, K, Q, J, T, `9`, `8`, `7`, `6`, `5`, `4`, `3`, `2`)
-		== List(Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two)) //true
+	require(List(A, K, Q, J, T, `9`, `8`, `7`, `6`, `5`, `4`, `3`, `2`)
+		== List(Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two))
 
 	val aceSpades: Card = A♠
 
-	println(aceSpades.rank) //A
-	println(aceSpades.suit) //♠
-	println(aceSpades) // A♠
+	require(aceSpades.rank == A)
+	require(aceSpades.suit == ♠)
+	require(aceSpades.toString == "A♠")
 
-	println((Ace spades) == (A♠)) //true
+	require((Ace spades) == (A♠))
 
-	println((K♦) < (A♠)) // true
+	require((K♦) < (A♠))
 
 	val shortestHand: Hand = Hand(A♠)
 	val longestHand = Hand(K♥, Q♥, J♥, T♥, `9`♥, A♣, A♠)
 
-	println(Hand(A♠, A♥, A♦, A♣, K♠, K♥, K♦) < Hand(A♥, K♥, Q♥, J♥, T♥)) //true
+	require(Hand(A♠, A♥, A♦, A♣, K♠, K♥, K♦) < Hand(A♥, K♥, Q♥, J♥, T♥))
 	val combination: Combination = Hand(A ♠, A ♥, A ♦, A ♣, K ♠, K ♥, K ♦).toCombination
-	println(combination) //Quad As with K kicker
-	println(Hand(A♥, K♥, Q♥, J♥, T♥).toCombination) //Royal flush
+	require(combination.toString == "Quad As with K kicker")
+	require(Hand(A♥, K♥, Q♥, J♥, T♥).toCombination.toString == "Royal flush")
 
-	println(Hand(A♠, A♥, A♦, K♣, Q♠, J♥) > Hand(A♠, A♥, A♦, K♣)) //true
-	println(Hand(A♠, A♥, A♦, Q♠, J♥) < Hand(A♠, A♥, A♦, K♣)) //true
+	require(Hand(A♠, A♥, A♦, K♣, Q♠, J♥) > Hand(A♠, A♥, A♦, K♣))
+	require(Hand(A♠, A♥, A♦, Q♠, J♥) < Hand(A♠, A♥, A♦, K♣))
 
-	println(Hand(A♠, A♥, A♦, K♣, Q♠, J♥).toCombination == Hand(A♠, A♥, A♦, K♣, Q♠).toCombination) //true
-	println(!(
+	require(Hand(A♠, A♥, A♦, K♣, Q♠, J♥).toCombination == Hand(A♠, A♥, A♦, K♣, Q♠).toCombination)
+	require(!(
 		Hand(A♠, A♥, A♦, K♣, Q♠, J♥) > Hand(A♠, A♥, A♦, K♣, Q♠)
 		|| Hand(A♠, A♥, A♦, K♣, Q♠, J♥) < Hand(A♠, A♥, A♦, K♣, Q♠)
-		)) //true
+		))
 
 	Hand(`2`♠, `2`♥, A♠, K♠, Q♠, J♥, `9`♥) match {
 		case Pair(`2`, highestKicker :: _) =>
-			println(s"A pair of 2s with $highestKicker kicker and so on") //A pair of 2s with A kicker and so on
+			require(s"A pair of 2s with $highestKicker kicker and so on" == "A pair of 2s with A kicker and so on")
 	}
 }
